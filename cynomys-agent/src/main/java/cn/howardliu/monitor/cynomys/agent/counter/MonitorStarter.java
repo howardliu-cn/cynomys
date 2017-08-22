d7c57cf9f10933a33cdde8b68afcb81b9e0f0d29
howardliu-cn <liuxinghao1988@gmail.com>
2017-08-22 11:22:19 +0800

    use thread to wait for START signal

--- cynomys-agent/src/main/java/cn/howardliu/monitor/cynomys/agent/counter/MonitorStarter.java
+++ cynomys-agent/src/main/java/cn/howardliu/monitor/cynomys/agent/counter/MonitorStarter.java
@@ -3,6 +3,9 @@ package cn.howardliu.monitor.cynomys.agent.counter;
 import cn.howardliu.monitor.cynomys.agent.conf.Parameters;
 import cn.howardliu.monitor.cynomys.agent.dto.JavaInformations;
 import cn.howardliu.monitor.cynomys.agent.handler.MonitorChecker;
+import cn.howardliu.monitor.cynomys.common.LaunchLatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static cn.howardliu.monitor.cynomys.agent.common.Constant.started;
 import static cn.howardliu.monitor.cynomys.common.Constant.SERVER_PORT;
@@ -15,20 +18,34 @@ import static cn.howardliu.monitor.cynomys.common.Constant.SERVER_PORT;
  * @since 0.0.1
  */
 public class MonitorStarter {
-    public static void run() {
+    private static final Logger logger = LoggerFactory.getLogger(MonitorStarter.class);
+
+    public static synchronized void run() {
         if (!started) {
-            JavaInformations javaInformations = JavaInformations.instance(true);
-            Parameters.initialize();
-            int port;
-            if (javaInformations.getTomcatInformationsList().isEmpty()) {
-                // TODO read port use System.getProperty(String, String)
-                port = Integer.valueOf(System.getProperty("server.port", SERVER_PORT + ""));
-            } else {
-                port = Integer.valueOf(javaInformations.getTomcatInformationsList().get(0).getHttpPort());
-            }
-            SERVER_PORT = port;
-            SLACounter.init();
-            new MonitorChecker(port).startHealth("Active");
+            Thread thread = new Thread(() -> {
+                JavaInformations javaInformations = JavaInformations.instance(true);
+                Parameters.initialize();
+                int port;
+                if (javaInformations.getTomcatInformationsList().isEmpty()) {
+                    port = Integer.valueOf(System.getProperty("server.port", SERVER_PORT + ""));
+                } else {
+                    port = Integer.valueOf(javaInformations.getTomcatInformationsList().get(0).getHttpPort());
+                }
+                SERVER_PORT = port;
+                SLACounter.init();
+
+                try {
+                    LaunchLatch.LATCH.waitForMillis(120_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    logger.error("LaunchLatch was interrupted!", e);
+                }
+                new MonitorChecker(port).startHealth("Active");
+            });
+            thread.setName("monitor-starter-thread");
+            thread.setDaemon(true);
+            thread.start();
+
             started = true;
         }
     }
